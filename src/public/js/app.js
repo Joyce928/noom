22949e67f0c0ade3588d1370eab84049d5385a0a
Joyce928 <92929006+Joyce928@users.noreply.github.com>
2023-05-27 01:29:17 +1000

    #2.7 Nicknames for Code Chanllenge

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,19 +8,58 @@ room.hidden = true;
 
 let roomName;
 
+function addMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("#msg input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = "";
+}
+function changeNickname(event) {
+  event.preventDefault();
+  const changeNameInput = room.querySelector("#name input");
+  socket.emit("change_nickname", changeNameInput.value);
+}
+
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const msgForm = room.querySelector("#msg");
+  msgForm.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom);
-  roomName = input.value;
-  input.value = "";
+  const roomNameInput = form.querySelector("#roomName");
+  const nickNameInput = welcome.querySelector("#name");
+  socket.emit("enter_room", roomNameInput.value, nickNameInput.value, showRoom);
+  roomName = roomNameInput.value;
+  roomNameInput.value = "";
+  const changeNameInput = room.querySelector("#name input");
+  changeNameInput.value = nickNameInput.value;
+  const changeNicknameForm = room.querySelector("form");
+  changeNicknameForm.addEventListener("submit", changeNickname);
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", (user) => {
+  addMessage(`${user} arrived!`);
+});
+
+socket.on("bye", (left) => {
+  addMessage(`${left} left.`);
+});
+
+socket.on("new_message", addMessage);
